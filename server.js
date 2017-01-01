fceb64e1dbe51fbc34a114e4594c0aca6fc1fc40
Spencer Sharpe <jspencersharpe@gmail.com>
2016-12-31 21:01:44 -0600

    remove lodash

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const _ = require('lodash');
 const nbaService = require('./services/nba.service.js');
 const ncaaService = require('./services/ncaa.service.js');
 
