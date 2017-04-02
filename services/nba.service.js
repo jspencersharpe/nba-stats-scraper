937cc5d3a5b2e798a5052136f95c6d63adfe002a
Spencer Sharpe <jspencersharpe@gmail.com>
2017-04-02 17:08:07 -0500

    Refactor scraping calls to promises

--- services/nba.service.js
+++ services/nba.service.js
@@ -4,78 +4,80 @@ const cheerio = require('cheerio');
 
 var exports = module.exports = {};
 
-exports.getNBAData = (url, callback) => {
+exports.getNBAData = (url) => {
   let self = this;
-  request(url, (error, response, html) => {
-    if (!error) {
-      let $ = cheerio.load(html);
-      self.playerInfo = [];
-      self.teamInfo = [];
-      self.imagesArr = [];
-      self.idArr = [];
-      $('#logo').each(function(index, value) {
-        let img = $('img');
-        let teamLogo = img[0].attribs.src;
-        self.teamInfo = {
-          teamLogo: teamLogo
-        };
-      });
-      $('.stats-table.season-averages tr').each(function(index, value) {
-        let name = $('.playerName', this).text(),
-          playerLink = $('.playerName a'),
-          playerImg = $('.player-name__inner-wrapper img'),
-          num = $('.playerNumber', this).text(),
-          pos = $('.playerPosition', this).text(),
-          gp = $('.gp', this).text(),
-          pts = $('.pts', this).text(),
-          fg_pct = $('.fg_pct', this).text(),
-          fg3_pct = $('.fg3_pct', this).text(),
-          ft_pct = $('.ft_pct', this).text(),
-          oreb = $('.oreb', this).text(),
-          dreb = $('.dreb', this).text(),
-          ast = $('.ast', this).text(),
-          reb = $('.reb', this).text(),
-          stl = $('.stl', this).text(),
-          tov = $('.tov', this).text(),
-          pf = $('.pf', this).text();
-        self.idArr.push(playerLink[index].attribs.href);
-        self.imagesArr.push(playerImg[index].attribs.src);
-        let data = {
-          name: name,
-          gp: gp,
-          num: num,
-          pos: pos,
-          pts: pts,
-          fg_pct: fg_pct,
-          fg3_pct: fg3_pct,
-          ft_pct: ft_pct,
-          ast: ast,
-          reb: reb,
-          oreb: oreb,
-          dreb: dreb,
-          stl: stl,
-          tov: tov,
-          pf: pf
-        };
-        self.playerInfo.push(data);
-      });
-      self.imagesArr.unshift('');
-      self.idArr.unshift('');
-      let idArr = self.idArr.map((x) => {
-        let splt = x.split('/');
-        return splt[4];
-      });
-      self.playerInfo.forEach((value, index) => {
-        value.pic = self.imagesArr[index];
-        value.playerId = idArr[index];
-        return value;
-      });
-      let teamData = {
-        playerData: self.playerInfo,
-        teamData: self.teamInfo
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      if (!error) {
+        let $ = cheerio.load(html);
+        self.playerInfo = [];
+        self.teamInfo = [];
+        self.imagesArr = [];
+        self.idArr = [];
+        $('#logo').each(function(index, value) {
+          let img = $('img');
+          let teamLogo = img[0].attribs.src;
+          self.teamInfo = {
+            teamLogo: teamLogo
+          };
+        });
+        $('.stats-table.season-averages tr').each(function(index, value) {
+          let name = $('.playerName', this).text(),
+            playerLink = $('.playerName a'),
+            playerImg = $('.player-name__inner-wrapper img'),
+            num = $('.playerNumber', this).text(),
+            pos = $('.playerPosition', this).text(),
+            gp = $('.gp', this).text(),
+            pts = $('.pts', this).text(),
+            fg_pct = $('.fg_pct', this).text(),
+            fg3_pct = $('.fg3_pct', this).text(),
+            ft_pct = $('.ft_pct', this).text(),
+            oreb = $('.oreb', this).text(),
+            dreb = $('.dreb', this).text(),
+            ast = $('.ast', this).text(),
+            reb = $('.reb', this).text(),
+            stl = $('.stl', this).text(),
+            tov = $('.tov', this).text(),
+            pf = $('.pf', this).text();
+          self.idArr.push(playerLink[index].attribs.href);
+          self.imagesArr.push(playerImg[index].attribs.src);
+          let data = {
+            name: name,
+            gp: gp,
+            num: num,
+            pos: pos,
+            pts: pts,
+            fg_pct: fg_pct,
+            fg3_pct: fg3_pct,
+            ft_pct: ft_pct,
+            ast: ast,
+            reb: reb,
+            oreb: oreb,
+            dreb: dreb,
+            stl: stl,
+            tov: tov,
+            pf: pf
+          };
+          self.playerInfo.push(data);
+        });
+        self.imagesArr.unshift('');
+        self.idArr.unshift('');
+        let idArr = self.idArr.map((x) => {
+          let splt = x.split('/');
+          return splt[4];
+        });
+        self.playerInfo.forEach((value, index) => {
+          value.pic = self.imagesArr[index];
+          value.playerId = idArr[index];
+          return value;
+        });
+        let teamData = {
+          playerData: self.playerInfo,
+          teamData: self.teamInfo
+        }
+        resolve(teamData);
       }
-      return callback(teamData);
-    }
+    });
   });
 }
 
--- services/teams.service.js
+++ services/teams.service.js
@@ -1,27 +1,30 @@
 const request = require('request');
+var Promise = require("promise");
 const cheerio = require('cheerio');
 
 var exports = module.exports = {};
 
-exports.getTeamList = (url, callback) => {
-  let self = this;
-  request(url, (error, response, html) => {
-    if (!error) {
-      let $ = cheerio.load(html);
-      self.teamInfo = [];
+exports.getTeamList = (url) => {
+  return new Promise((resolve, reject) => {
+    let self = this;
+    request(url, (error, response, html) => {
+      if (!error) {
+        let $ = cheerio.load(html);
+        self.teamInfo = [];
 
-      $('.team__list_wrapper').each(function(index, value) {
-        let arr = $('.team__list img', this);
-        arr.each(function(index, value) {
-          let team = {
-            url: value.attribs.src,
-            name: value.attribs.alt
-          };
-          self.teamInfo.push(team);
+        $('.team__list_wrapper').each(function(index, value) {
+          let arr = $('.team__list img', this);
+          arr.each(function(index, value) {
+            let team = {
+              url: value.attribs.src,
+              name: value.attribs.alt
+            };
+            self.teamInfo.push(team);
+          });
         });
-      });
+        resolve(self.teamInfo);
+      }
+    });
+  })
 
-      return callback(self.teamInfo);
-    }
-  });
 }
