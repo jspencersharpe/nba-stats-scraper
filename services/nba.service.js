d12a93c168e4312c2d7ed337f0805f14bfa046f0
Spencer Sharpe <jspencersharpe@gmail.com>
2017-04-03 21:24:52 -0500

    Refactor. Removes unnecessary variables

--- services/nba.service.js
+++ services/nba.service.js
@@ -22,41 +22,26 @@ exports.getNBAData = (url) => {
           };
         });
         $('.stats-table.season-averages tr').each(function(index, value) {
-          let name = $('.playerName', this).text(),
-            playerLink = $('.playerName a'),
-            playerImg = $('.player-name__inner-wrapper img'),
-            num = $('.playerNumber', this).text(),
-            pos = $('.playerPosition', this).text(),
-            gp = $('.gp', this).text(),
-            pts = $('.pts', this).text(),
-            fg_pct = $('.fg_pct', this).text(),
-            fg3_pct = $('.fg3_pct', this).text(),
-            ft_pct = $('.ft_pct', this).text(),
-            oreb = $('.oreb', this).text(),
-            dreb = $('.dreb', this).text(),
-            ast = $('.ast', this).text(),
-            reb = $('.reb', this).text(),
-            stl = $('.stl', this).text(),
-            tov = $('.tov', this).text(),
-            pf = $('.pf', this).text();
+          let playerLink = $('.playerName a');
+          let playerImg = $('.player-name__inner-wrapper img');
           self.idArr.push(playerLink[index].attribs.href);
           self.imagesArr.push(playerImg[index].attribs.src);
           let data = {
-            name: name,
-            gp: gp,
-            num: num,
-            pos: pos,
-            pts: pts,
-            fg_pct: fg_pct,
-            fg3_pct: fg3_pct,
-            ft_pct: ft_pct,
-            ast: ast,
-            reb: reb,
-            oreb: oreb,
-            dreb: dreb,
-            stl: stl,
-            tov: tov,
-            pf: pf
+            name: $('.playerName', this).text(),
+            gp: $('.gp', this).text(),
+            num: $('.playerNumber', this).text(),
+            pos: $('.playerPosition', this).text(),
+            pts: $('.pts', this).text(),
+            fg_pct: $('.fg_pct', this).text(),
+            fg3_pct: $('.fg3_pct', this).text(),
+            ft_pct: $('.ft_pct', this).text(),
+            ast: $('.ast', this).text(),
+            reb: $('.reb', this).text(),
+            oreb: $('.oreb', this).text(),
+            dreb: $('.dreb', this).text(),
+            stl: $('.stl', this).text(),
+            tov: $('.tov', this).text(),
+            pf: $('.pf', this).text()
           };
           self.playerInfo.push(data);
         });
