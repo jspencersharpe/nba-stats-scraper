110999758210d2ba0f4e91fadefdb7e9a7b0e128
Spencer Sharpe <jspencersharpe@gmail.com>
2019-02-18 13:38:47 -0600

    Remove NCAA pages and features

--- helpers/format.js
+++ helpers/format.js
@@ -33,13 +33,6 @@ export function formatTeamData (data) {
     }
   });
 
-  data.push({
-    route: 'ncaa',
-    btnText: 'View Teams',
-    className: 'ncaa',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/NCAA_logo.svg/1200px-NCAA_logo.svg.png'
-  });
-
   return data;
 }
 
@@ -63,23 +56,3 @@ export function formatPlayerData(data) {
     age: filtered[6]
   };
 }
-
-export function formatSchoolList(data) {
-  for (let school of data) {
-    let name = school.teamName.split('-');
-    school.teamName = formatStrings(name).join(' ');
-    school.link = `http://www.espn.com${school.link}`;
-  }
-  return data;
-}
-
-const formatStrings = (team) => {
-  for (let i = 0; i < team.length; i++) {
-    if (team[i] === '%26') { team[i] = '&'; }
-    if (team[i] === 'a%26m') { team[i] = 'A&M'; }
-    if (team[i] === 'jos%C3%A9') { team[i] = 'Jose'; }
-    if (team[i] === 'a%26t') { team[i] = 'A&T'; }
-    team[i] = team[i].charAt(0).toUpperCase() + team[i].slice(1);
-  }
-  return team;
-};
