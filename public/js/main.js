2546160bd0fc16fa9273944cde97a3d0efe80652
Spencer Sharpe <jspencersharpe@gmail.com>
2019-03-02 22:07:54 -0600

    Removes loading feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,10 @@
 $(document).ready(function() {
-  var $team = $('.team');
-  var $loading = $('.loading');
-  var $body = $('body');
-  var $nav = $('nav');
-  var $container = $('.container-fluid');
-  var $link = $('a');
   var $back = $('#back');
-  var $teamImg = $('.rounded-circle');
-  var $teamButton = $('#teamButton');
-
-  $teamImg.on('click', function() {
-    hideElements($team, $nav);
-    toggleLoadingClass($body, $loading);
-  });
-
-  $teamButton.on('click', function() {
-    hideElements($team, $nav);
-    toggleLoadingClass($body, $loading);
-  });
-
-  $link.on('click', function() {
-    hideElements($nav, $container);
-    toggleLoadingClass($body, $loading);
-  });
 
   $back.on('click', function() {
     goBack();
   });
 });
-
-function hideElements(teams, nav) {
-  teams.hide();
-  nav.hide();
-}
-
-function toggleLoadingClass(body, loading) {
-  body.addClass('background');
-  loading.removeClass('hidden');
-}
 
 function goBack() {
   window.history.back();
--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    const teamsList = formatTeamData(teamConfig);
-    res.render('index', {
-        teamsList: teamsList
-    });
+  const teamsList = formatTeamData(teamConfig);
+  res.render('index', {
+    teamsList: teamsList
+  });
 });
 
 app.get('/:name([\\w-]{0,}?):teamId(\\d+)/', (req, res) => {
-  let { name, teamId} = req.params;
+  let { name, teamId } = req.params;
   name = name.replace(/-/g, "");
   const url = `https://stats.nba.com/stats/commonteamroster?LeagueID=00&Season=2018-19&TeamID=${teamId}`;
   const imgUrl = `https://www.nba.com/assets/logos/teams/primary/web/${name}.svg`
@@ -25,7 +25,9 @@ app.get('/:name([\\w-]{0,}?):teamId(\\d+)/', (req, res) => {
     .then(response => {
       let playerData = formatStat(response);
       const currentTeam = teamConfig.find(t => t.teamId === teamId);
-      const teamData = Object.assign({}, currentTeam, {imgUrl: imgUrl});
+      const teamData = Object.assign({}, currentTeam, {
+        imgUrl: imgUrl
+      });
 
       res.render('stats', {
         playerData: playerData,
@@ -41,27 +43,27 @@ app.get('/player/:playerId', (req, res) => {
   const url = `https://stats.nba.com/stats/playercareerstats?PlayerID=${playerId}&PerMode=PerGame`;
   const infoUrl = `https://stats.nba.com/stats/commonplayerinfo?LeagueID=00&PlayerID=${playerId}`
 
-    let playerStats = getNBAData(url).then(response => {
-        const data = response.resultSets[0]
-        return formatPlayerStats(data);
+  let playerStats = getNBAData(url).then(response => {
+    const data = response.resultSets[0]
+    return formatPlayerStats(data);
+  });
+
+  let playerInfo = getNBAData(infoUrl).then(response => {
+    const data = response.resultSets[0];
+    return formatPlayerData(data);
+  });
+
+  Promise.all([playerStats, playerInfo]).then(response => {
+    const [stats, playerInfo] = response;
+
+    res.render('player', {
+      stats: stats,
+      playerInfo: playerInfo
     });
 
-    let playerInfo = getNBAData(infoUrl).then(response => {
-        const data = response.resultSets[0];
-        return formatPlayerData(data);
-    });
-
-    Promise.all([playerStats, playerInfo]).then(response => {
-        const [ stats, playerInfo ] = response;
-
-        res.render('player', {
-            stats: stats,
-            playerInfo: playerInfo
-        });
-
-    }).catch(() => {
-      res.render('404', {});
-    });
+  }).catch(() => {
+    res.render('404', {});
+  });
 });
 
 app.listen(process.env.PORT || 3333, () => console.log('Running on 3333'));
