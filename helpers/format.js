4a1f8cfa01ec780cc41e7ad79ebe76daab53b97a
Spencer Sharpe <jspencersharpe@gmail.com>
2019-03-02 22:16:50 -0600

    remove unused styles

--- helpers/format.js
+++ helpers/format.js
@@ -1,45 +1,45 @@
 export function formatStat(data) {
-    const { headers, rowSet } = data.resultSets[0];
-    let players = [];
+  const { headers, rowSet } = data.resultSets[0];
+  let players = [];
 
-    for (var i = 0; i < rowSet.length; i++) {
-        const data = rowSet[i];
+  for (var i = 0; i < rowSet.length; i++) {
+      const data = rowSet[i];
 
-        const player = Object.assign(...headers.map((k, i) => ({[k]: data[i]})))
+      const player = Object.assign(...headers.map((k, i) => ({[k]: data[i]})))
 
-        players.push(player);
-    }
+      players.push(player);
+  }
 
-    return players;
+  return players;
 }
 
 export function formatTeamData (data) {
-    return data.map(team => {
-        team.imgUrl = `https://www.nba.com/assets/logos/teams/primary/web/${team.tricode}.svg`
+  return data.map(team => {
+      team.imgUrl = `https://www.nba.com/assets/logos/teams/primary/web/${team.tricode}.svg`
 
-        return team;
-    });
+      return team;
+  });
 }
 
 export function formatPlayerStats(data) {
-    const { headers, rowSet } = data;
-    let seasons = [];
+  const { headers, rowSet } = data;
+  let seasons = [];
 
-    for (var i = 0; i < rowSet.length; i++) {
-        const data = rowSet[i];
-        const season = Object.assign(...headers.map((k, i) => ({[k]: data[i]})))
+  for (var i = 0; i < rowSet.length; i++) {
+      const data = rowSet[i];
+      const season = Object.assign(...headers.map((k, i) => ({[k]: data[i]})))
 
-        seasons.push(season);
-    }
+      seasons.push(season);
+  }
 
-    return seasons.reverse();
+  return seasons.reverse();
 }
 
 export function formatPlayerData(data) {
-    const { headers, rowSet } = data;
-    let info = rowSet[0];
+  const { headers, rowSet } = data;
+  let info = rowSet[0];
 
-    const playerData = Object.assign(...headers.map((k, i) => ({[k]: info[i]})))
+  const playerData = Object.assign(...headers.map((k, i) => ({[k]: info[i]})))
 
-    return playerData;
+  return playerData;
 }
--- services/nba.service.js
+++ services/nba.service.js
@@ -2,24 +2,24 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 const mockBrowserHeaders = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Connection': 'keep-alive',
-    'Host': 'stats.nba.com',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+  'Accept-Encoding': 'gzip, deflate',
+  'Accept-Language': 'en-US,en;q=0.8',
+  'Connection': 'keep-alive',
+  'Host': 'stats.nba.com',
+  'Upgrade-Insecure-Requests': '1',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 };
 
 export default function getNBAData(url) {
   return fetch(url, {
-      method: 'GET',
-      headers: mockBrowserHeaders
+    method: 'GET',
+    headers: mockBrowserHeaders
   }).then(function (response) {
-      if (response.status >= 400) {
-        throw new Error("Bad response from server");
-      }
-      return response.json();
-    })
+    if (response.status >= 400) {
+      throw new Error("Bad response from server");
+    }
+    return response.json();
+  });
 }
 
